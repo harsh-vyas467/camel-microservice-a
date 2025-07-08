8cfc4c42b0b58adde67bcdab1f9cccd786b6ae2d
Harsh Vyas <harsh.vyas467@gmail.com>
2025-07-08 23:51:36 +0530

    send any file recieved inside json folder to activeMQ

--- src/main/java/com/microservices/camel_microservice_a/routes/c/ActiveMqSenderRouter.java
+++ src/main/java/com/microservices/camel_microservice_a/routes/c/ActiveMqSenderRouter.java
@@ -9,8 +9,13 @@ public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         // to put regular messages on que
-        from("timer:active-mq-timer?period=10000")
-                .transform().constant("My message for Active MQ")
+//        from("timer:active-mq-timer?period=10000")
+//                .transform().constant("My message for Active MQ")
+//                .log("${body}")
+//                .to("activemq:my-activemq-queue");
+
+//          to send file to activemq
+        from("file:files/json")
                 .log("${body}")
                 .to("activemq:my-activemq-queue");
     }
