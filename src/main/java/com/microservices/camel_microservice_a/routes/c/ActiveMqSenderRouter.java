14986d8c5484f99aef67ee5d30d1792ac2090ef1
Harsh Vyas <harsh.vyas467@gmail.com>
2025-07-08 23:05:51 +0530

    created two routes 1. to copy files from input folder to output 2. creted producer for activemq 3. done setup for activemq on docker

---
+++ src/main/java/com/microservices/camel_microservice_a/routes/c/ActiveMqSenderRouter.java
@@ -0,0 +1,17 @@
+package com.microservices.camel_microservice_a.routes.c;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // to put regular messages on que
+        from("timer:active-mq-timer?period=10000")
+                .transform().constant("My message for Active MQ")
+                .log("${body}")
+                .to("activemq:my-activemq-queue");
+    }
+}
