a51ad2ce543385da4a75d88d488614d43885d010
Harsh Vyas <harsh.vyas467@gmail.com>
2025-07-10 18:39:00 +0530

    kafka setup on docker using docker-compose and kafka routes and properties config

---
+++ src/main/java/com/microservices/camel_microservice_a/routes/d/KafkaSenderJsonRouter.java
@@ -0,0 +1,16 @@
+package com.microservices.camel_microservice_a.routes.d;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaSenderJsonRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+//          to send file to kafka
+        from("file:files/json")
+                .log("${body}")
+                .to("kafka:myKafkaTopic");
+    }
+}
